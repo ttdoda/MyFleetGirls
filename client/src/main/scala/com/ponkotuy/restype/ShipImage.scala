fb63ac5c88ed8163355b32b10bb71242ba738a19
kPherox <admin@mail.kr-kp.com>
2018-09-12 02:34:09 +0900

    Change SoundMP3 extends ResType to Resources

--- client/src/main/scala/com/ponkotuy/restype/ShipImage.scala
+++ client/src/main/scala/com/ponkotuy/restype/ShipImage.scala
@@ -16,7 +16,7 @@ case object ShipImage extends Resources {
     val ver = q.uri.query.param("version").map(_.toInt).getOrElse(DefaultVer)
     parse(q.uri).filterNot { case (shipId, kind) => MFGHttp.existsImage2nd(shipId, kind, ver) }.map { case (shipId, kind) =>
       val png = allRead(q.responseContent)
-      FilePostable(s"/image/ship/$shipId/$kind/$ver", "image", 2, png, "png")
+      FilePostable(s"/image/ship/${shipId}/${kind}/${ver}", "image", 2, png, "png")
     }.toList
   }
 }
--- client/src/main/scala/com/ponkotuy/restype/SoundMP3.scala
+++ client/src/main/scala/com/ponkotuy/restype/SoundMP3.scala
@@ -2,40 +2,21 @@ package com.ponkotuy.restype
 
 import com.ponkotuy.http.MFGHttp
 import com.ponkotuy.parser.Query
-import com.ponkotuy.restype.ShipSWF.allRead
-import com.ponkotuy.util.Log
 
-import scala.util.Try
 import scala.util.matching.Regex
 
 /**
  * @author ponkotuy
  * DAte: 15/04/12.
  */
-case object SoundMP3 extends ResType {
-  override def regexp: Regex = """\A/kcs/sound/kc[a-z]+/[0-9]+\.mp3""".r
+case object SoundMP3 extends Resources {
+  def regexp: Regex = """\A/kcs/sound/kc([a-z]+)/(\d+).mp3""".r
 
-  override def postables(q: Query): Seq[Result] = {
-    SoundUrlId.parseURL(q.uri.toString())
-        .filterNot(MFGHttp.existsSound)
-        .map { case SoundUrlId(shipKey, soundId, ver) =>
+  def postables(q: Query): Seq[Result] = {
+    val ver = q.uri.query.param("version").map(_.toInt).getOrElse(DefaultVer)
+    parse(q.uri).filterNot { case (soundId, shipKey) => MFGHttp.existsSound(shipKey, soundId, ver) }.map { case (soundId, shipKey) =>
       val sound = allRead(q.responseContent)
       FilePostable(s"/mp3/kc/${shipKey}/${soundId}/${ver}", "sound", 2, sound, "mp3")
     }.toList
-  }
-}
-
-case class SoundUrlId(shipKey: String, soundId: Int, version: Int)
-
-object SoundUrlId extends Log {
-  val Pattern = """.*/kcs/sound/kc([a-z]+)/(\d+).mp3\?version\=(\d+)""".r
-  val NoVerPattern = """.*/kcs/sound/kc([a-z]+)/(\d+).mp3""".r
-
-  def parseURL(url: String): Option[SoundUrlId] = {
-    url match {
-      case Pattern(ship, sound, version) => Try { SoundUrlId(ship, sound.toInt, version.toInt) }.toOption
-      case NoVerPattern(ship, sound) => Try { SoundUrlId(ship, sound.toInt, 0) }.toOption
-      case _ => logger.error("Can't parse to SoundResource URL:{}",url); None
-    }
   }
 }
