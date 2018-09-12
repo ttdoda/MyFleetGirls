abf359e3afa8d5a7c0a3b37dd04fa4bc36cfdf05
kPherox <admin@mail.kr-kp.com>
2018-09-13 00:08:25 +0900

    Add controller for post map image

--- server/app/controllers/PostFile.scala
+++ server/app/controllers/PostFile.scala
@@ -5,8 +5,7 @@ import javax.inject.Inject
 
 import com.ponkotuy.value.ShipIds
 import controllers.Common._
-import models.db
-import models.db.{CellPosition, MapImage}
+import models.db.{ShipImage2nd, MapImage2nd, ShipSound, MasterShipBase}
 import play.api.mvc._
 import scalikejdbc._
 
@@ -28,11 +27,11 @@ class PostFile @Inject()(implicit val ec: ExecutionContext) extends Controller {
       request.body.file("image") match {
         case Some(ref) =>
           if(ShipIds.isEnemy(shipId)) Ok("Unnecessary Enemy")
-          else if(db.ShipImage2nd.find(shipId, kind, version).isDefined) Ok("Already exists")
+          else if(ShipImage2nd.find(shipId, kind, version).isDefined) Ok("Already exists")
           else {
             val pngFile = ref.ref.file
             val image = readAll(new FileInputStream(pngFile))
-            db.ShipImage2nd.create(shipId, image, auth.id, kind, version)
+            ShipImage2nd.create(shipId, image, auth.id, kind, version)
             Ok("Success")
           }
         case _ => BadRequest("Need ship image")
@@ -69,6 +68,29 @@ class PostFile @Inject()(implicit val ec: ExecutionContext) extends Controller {
     }
   }*/
 
+  /**
+   * 二期HTML5版海域画像
+   *
+   * 一期はswfファイルにCellPositionも含まれていたが二期では別のjsonなのでCellPositionに当たるものはcontroller.Post.cellPositionへ移動する
+   * また、ここで保存する画像はスプライトなので実際に使用する際にはMapDataを利用して描写すること
+   */
+  def map(areaId: Int, infoNo: Int, version: Int) = Action.async(parse.multipartFormData) { request =>
+    val form = request.body.asFormUrlEncoded
+    authentication(form) { auth =>
+      request.body.file("map") match {
+        case Some(ref) =>
+          if(MapImage2nd.find(areaId, infoNo, version.toShort).isDefined) Ok("Already exists")
+          else {
+            val pngFile = ref.ref.file
+            val image = readAll(new FileInputStream(pngFile))
+            MapImage2nd.create(areaId, infoNo, image, version.toShort)
+            Ok("Success")
+          }
+        case _ => BadRequest("Need ship image")
+      }
+    }
+  }
+
   def sound(shipKey: String, soundId: Int, version: Int) = Action.async(parse.multipartFormData) { request =>
     val form = request.body.asFormUrlEncoded
     authentication(form) { auth =>
@@ -78,7 +100,7 @@ class PostFile @Inject()(implicit val ec: ExecutionContext) extends Controller {
             val mp3File = ref.ref.file
             val sound = readAll(new FileInputStream(mp3File))
             try {
-              db.ShipSound.create(ship.id, soundId, version, sound)
+              ShipSound.create(ship.id, soundId, version, sound)
               Ok("Success")
             } catch {
               case e: Exception => Ok("Already exists")
@@ -89,8 +111,8 @@ class PostFile @Inject()(implicit val ec: ExecutionContext) extends Controller {
     }
   }
 
-  private def findKey(key: String)(f: db.MasterShipBase => Result) = {
-    db.MasterShipBase.findByFilename(key) match {
+  private def findKey(key: String)(f: MasterShipBase => Result) = {
+    MasterShipBase.findByFilename(key) match {
       case Some(ship) => f(ship)
       case None => Ok("Is enemy, wrong filename or Not found master data")
     }
