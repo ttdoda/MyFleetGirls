d96e4cc8a5d9ebcc22f6c7692425b0c66b719c13
kPherox <admin@mail.kr-kp.com>
2018-10-02 16:48:35 +0900

    Use `play.assets.encodings` instead of custom assets controller
    Use `AssetsFinder` instead of path parameter

--- server/app/controllers/MyAssets.scala
+++ server/app/controllers/MyAssets.scala
@@ -4,7 +4,7 @@ import javax.inject.Inject
 
 import play.api.mvc.{Action, BaseController, ControllerComponents}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 import com.ponkotuy.value.KCServer
 
@@ -14,19 +14,6 @@ import com.ponkotuy.value.KCServer
  */
 class MyAssets @Inject()(val controllerComponents: ControllerComponents, implicit val ec: ExecutionContext) extends BaseController {
   val pacDefaultPort = 8080
-
-
-  def at(path: String, file: String, aggressiveCaching: Boolean = false) = Action.async { implicit req =>
-    val accepts = req.headers.get(ACCEPT_ENCODING).map(_.split(",").map(_.stripMargin)).getOrElse(Array())
-    if(accepts.contains("pack200-gzip")) {
-      Assets.at(path, file + ".pack.gz").apply(req).flatMap { result =>
-        if(result.header.status >= 400) Assets.at(path, file, aggressiveCaching).apply(req)
-        else Future(result.withHeaders(CONTENT_ENCODING -> "pack200-gzip"))
-      }
-    } else {
-      controllers.Assets.at(path, file, aggressiveCaching).apply(req)
-    }
-  }
 
   def pacDynamicScript(port: Int = pacDefaultPort) = Action {
     Ok(views.html.proxy.render(KCServer.values,port)).as("application/x-ns-proxy-autoconfig")
