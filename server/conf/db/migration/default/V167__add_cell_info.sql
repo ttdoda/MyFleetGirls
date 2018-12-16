57312a1a44cc64c843f16fa49022b9d27897dc97
kPherox <admin@mail.kr-kp.com>
2018-12-16 20:39:14 +0900

    7-2 海域情報追加

---
+++ server/conf/db/migration/default/V167__add_cell_info.sql
@@ -0,0 +1,18 @@
+
+insert into cell_info values
+  (7, 2, 1, 'A', true, false),
+  (7, 2, 2, 'B', true, false),
+  (7, 2, 3, 'C', true, false),
+  (7, 2, 4, 'D', false, false),
+  (7, 2, 5, 'E', false, false),
+  (7, 2, 6, 'F', false, false),
+  (7, 2, 7, 'G', false, true),
+  (7, 2, 8, 'B', false, false),
+  (7, 2, 9, 'C', false, false),
+  (7, 2, 10, 'H', false, false),
+  (7, 2, 11, 'I', false, false),
+  (7, 2, 12, 'J', false, false),
+  (7, 2, 13, 'K', false, false),
+  (7, 2, 14, 'L', false, false),
+  (7, 2, 15, 'M', false, true),
+  (7, 2, 16, 'I', false, false);
